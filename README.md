8d79f054ac88718f1d25b4b40c85cadd7ef85f43
Lettergram <Lettergramm@gmail.com>
2014-07-06 18:29:31 -0500

    some working code

---
+++ README.md
@@ -0,0 +1,6 @@
+Vacation, Road Trip Algorithm
+===========
+
+This was written by Austin Walters for austingwalters.com
+
+I should add, it doesn't have to be just vactations, this is also applicable for truckers hehe
---
+++ roadtrip.py
@@ -0,0 +1,73 @@
+from random import randint
+
+
+optimalDistance = 270
+
+# Optimal Gas Stoppages
+def minimizeStoppingTime(gasStations):
+    
+    optimalPathCost = []
+    previousStation = []
+
+    # Base case
+    optimalPathCost.append(0)
+    previousStation.append(0)
+
+    for i in range(1, len(gasStations)):
+   
+        optimalPathCost.append(999999999)
+        previousStation.append(i)
+
+        for j in range(0, i - 1):
+
+            # Calculate penalty points from j to potential station
+            penalty = optimalPathCost[j]
+            penalty += penaltyFunction(gasStations[j], gasStations[i]);
+
+            # Check to make sure we have enough gas & minimize penalty
+            if optimalPathCost[i] > penalty and penalty >= 0:
+                optimalPathCost[i] = penalty
+                previousStation[i] = j
+
+    # This part just traces the least penalizing path 
+    # Starting from the finish to the start
+    station = len(previousStation) - 1
+    optimizedPath = []
+    while station > 0:
+        optimizedPath.append(station)
+        station = previousStation[station]
+    optimizedPath.append(gasStations[0])
+
+    return optimizedPath
+
+# Penalization Function
+def penaltyFunction(sourceStation, destinationStation):
+    pen = sourceStation - destinationStation + optimalDistance
+    return pen * pen * pen
+
+
+# Generates Gas Stations at Random Mile Markers
+def generateGasStations(startLoc, endLoc):
+
+    gasStations = [0, endLoc]
+
+    for i in range(startLoc, endLoc, optimalDistance):
+        divisor = randint(2, 50)
+        for j in range(i, i + optimalDistance, optimalDistance / divisor):
+            gasStations.append(randint(i + j, i + j + optimalDistance) % 2907)
+
+    return sorted(gasStations)
+
+
+# Distance from New York to San Francisco, just for kicks                                         
+gasStationMileMarker = generateGasStations(0, 2908)
+
+optimizedPath =  minimizeStoppingTime(gasStationMileMarker)
+stop = 0
+
+for i in reversed(optimizedPath):
+    str = "Stop #%2d" % (stop)
+    str += " at Gas Station #%4d " % i
+    str += "located at mile marker %4d" % (gasStationMileMarker[i])
+    print str
+    stop+=1
